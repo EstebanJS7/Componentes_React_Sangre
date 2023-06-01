d30b8b384d7b100c6ff918c8f9221d71fa2d57d9
Esteban Jimenez <estebandjs7@gmail.com>
2023-06-01 09:23:46 -0400

    formulario perfil estilizado

--- src/views/Perfil.jsx
+++ src/views/Perfil.jsx
@@ -1,38 +1,49 @@
 import React from "react";
 
 const Perfil = () => {
+  const styleimg = {
+    width: '120px',
+    height: '180px',
+  }
+
+  const boldText ={
+    fontWeight: 'bold',
+  }
   return (
     <div className="container">
-      <div className="row justify-content-center mt-1">
+      <div className="row justify-content-center mt-1" >
         <div className="col-md-4">
           <div className="card">
             <div className="card-header">
               <h3 className="text-center">Alan TORO</h3>
             </div>
             <div className="card-body">
-              <img
-                className="d-flex justify-content-center"
-                src=""
-                alt="foto de perfil"
-              />
+              <div className="rounded-circle border-primary d-flex justify-content-center mb-4" >
+                <img
+                  className="image-perfil py-4"
+                  src="src/views/perfil.png"
+                  alt="foto de perfil"
+                  style={styleimg}
+                />
+              </div>
               <p className="d-flex justify-content-between">
-                <span>Fecha de nacimiento</span>
+                <span style={boldText}>Fecha de nacimiento: </span>
                 <span> </span>
               </p>
               <p className="d-flex justify-content-between">
-                <span>Email </span>
+                <span style={boldText}>Email: </span>
                 <span></span>
               </p>
               <p className="d-flex justify-content-between">
-                <span>Ultima vez donado: </span>
+                <span style={boldText}>Ultima vez donado: </span>
                 <span></span>
               </p>
               <p className="d-flex justify-content-between">
-                <span>Sexo: </span>
+                <span style={boldText}>Sexo: </span>
                 <span></span>
               </p>
               <p className="d-flex justify-content-between">
-                <span>C.I: </span>
+                <span style={boldText}>C.I: </span>
                 <span></span>
               </p>
             </div>
